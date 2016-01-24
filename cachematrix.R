8770be5ec7dbb5aba4bc686f54947a65c054ed9a
Chen He <chen.he@oracle.com>
2016-01-24 00:28:55 -0800

    Commit to the homework2 in week3 from Chen He

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  setInverMatrix <- function(inverseMatrix) m <<- inverseMatrix
+  getInverMatrix <- function() m
 
+  list(set = set, get = get,
+       setInverMatrix = setInverMatrix,
+       getInverMatrix = getInverMatrix)
 }
-
 
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverMatrix()
+  if(!is.null(m)) {
+    message("getting cached Matrix")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverMatrix(m)
+  m
 }
+
